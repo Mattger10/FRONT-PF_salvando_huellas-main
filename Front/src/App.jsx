6ee4a14e356fccd288d9dca4f396e4d9d8f8f1c1
Mattger10 <matiaszacariaz@hotmail.com>
2023-04-11 15:38:18 -0300

    first commit

---
+++ Front/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/js/dist/*.js'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     SALVANDO HUELLAS
+    </div>
+  )
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,1 @@
+# PF_salvando_huellas
