60e8a7073e46746e44d54b56d9e9cac6b5a02255
aleev97 <valverdealejandro327@gmail.com>
2023-04-12 01:14:41 -0300

    add (component/Home) : home creado

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import LandingPage from './components/LandingPage.jsx';
+import Home from './components/Home.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css'
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Routes>
       <Route path="/" element={<LandingPage />}/>
+      <Route path="/home" element ={<Home/>}/>
       </Routes>
     </div>
     </BrowserRouter>
