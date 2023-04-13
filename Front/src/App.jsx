2708fdd08ddc1847faa38ecbad2cb4da4517ba46
aleev97 <valverdealejandro327@gmail.com>
2023-04-13 02:54:24 -0300

    add (component/Donation) : componente para donar

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage/LandingPage.jsx';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Home from './components/Home/Home.jsx';
+import Donation from './components/Donations/Donations.jsx';
 
 import "./App.css";
 
@@ -14,6 +15,7 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <Routes>
+      <Route path="/donar" element={<Donation/>}/>
       <Route path="/" element={<LandingPage />}/>
       <Route path="/home" element ={<Home/>}/>
       </Routes>
---
+++ Front/src/components/Donations/Donations.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./Donation.module.css"
+import NavBar from "../NavBar/NavBar";
+
+export default function Donation() {
+
+    return (
+        <div>
+          <NavBar />
+          <h1 className={style.title} >SALVANDO HUELLAS!</h1>
+         <h5>Donar dinero o insumos de alimentos para perritos en adopción puede marcar una gran diferencia en la vida de estos animales necesitados. Muchas organizaciones sin fines de lucro dependen de la generosidad de la gente para rescatar, cuidar y encontrar hogares amorosos para perros abandonados y en situación de calle. Además, adoptar un perro puede tener un impacto positivo en la salud mental y física de las personas, y ayudar a reducir la sobrepoblación de animales en situación de calle. Donar es una manera fácil y efectiva de contribuir a la sociedad y mejorar la vida de los perritos en adopción.</h5>
+        </div>
+    )
+
+};
