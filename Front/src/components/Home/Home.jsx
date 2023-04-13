4e189315e97e3f1d5f0f9774b20f9edfcc2c7f4a
aleev97 <valverdealejandro327@gmail.com>
2023-04-13 01:45:13 -0300

    change (comp/home) : modifique style por problemas

--- Front/src/components/Home/Home.jsx
+++ Front/src/components/Home/Home.jsx
@@ -13,15 +13,15 @@ export default function Home() {
                 <img className={style.img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMHzZqXN5Oyq8D68x_x7jMVftbrCqI_aTIIA&usqp=CAU" alt="refugio" />
             </div>
 
-            <center className={style.iconsContainer} >
+            <div className={style.iconsContainer} >
                 <div>
                     <h5 className={style.follow} >siguenos en redes sociales:</h5>
                     <div className={style.icons} >
-                    <a href="https://www.facebook.com/profile.php?id=100079609239145" target="_blank"><img loading="lazy" alt="Síganos en Facebook" height="35" width="35" src=" https://1.bp.blogspot.com/-Pop-U7OywXs/YPhkfrImyiI/AAAAAAAAA1Y/UBnVfTK9j2U-bIUqwZYMNSQPvz_l06mbwCPcBGAYYCw/s0/facebook-icono.png" title="Síganos en Facebook" /></a>
-                    <a href="https://www.instagram.com/salvandohuellas9/" target="_blank"><img loading="lazy" alt="Síganos en Instagram" height="35" width="35" src="https://1.bp.blogspot.com/-VFfOISywV0c/YPhkeRXuRQI/AAAAAAAAA1M/L75S9Usg5AovunH2Y-VzqJbaaY1LuK3eACPcBGAYYCw/s0/Instagram-icono.png" title="Síganos en Instagram" /></a>
+                        <a href="https://www.facebook.com/profile.php?id=100079609239145" target="_blank"><img loading="lazy" alt="Síganos en Facebook" height="35" width="35" src=" https://1.bp.blogspot.com/-Pop-U7OywXs/YPhkfrImyiI/AAAAAAAAA1Y/UBnVfTK9j2U-bIUqwZYMNSQPvz_l06mbwCPcBGAYYCw/s0/facebook-icono.png" title="Síganos en Facebook" /></a>
+                        <a href="https://www.instagram.com/salvandohuellas9/" target="_blank"><img loading="lazy" alt="Síganos en Instagram" height="35" width="35" src="https://1.bp.blogspot.com/-VFfOISywV0c/YPhkeRXuRQI/AAAAAAAAA1M/L75S9Usg5AovunH2Y-VzqJbaaY1LuK3eACPcBGAYYCw/s0/Instagram-icono.png" title="Síganos en Instagram" /></a>
                     </div>
                 </div>
-            </center>
+            </div>
 
 
         </div>
