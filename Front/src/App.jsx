4c0234d0df5fa8968838eace2fe62f35cff2731e
Mattger10 <matiaszacariaz@hotmail.com>
2023-04-14 03:11:29 -0300

    cambio en app

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -4,8 +4,8 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Home from "./components/Home/Home.jsx";
 import Donation from "./components/Donations/Donations.jsx";
-import Shop from "./components/Shop/Shop.jsx";
 import Form from  "./components/FormAdoption/Form.jsx"
+import Shop from "./components/Shop/Shop.jsx";
 
 import "./App.css";
 
