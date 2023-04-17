d5852d797d65d00a777a6ec590b5f6fe6493ef20
aleev97 <valverdealejandro327@gmail.com>
2023-04-17 12:11:31 -0300

    change(comp/shop) : nuevo articulo

--- Front/src/components/Shop/Shop.jsx
+++ Front/src/components/Shop/Shop.jsx
@@ -22,9 +22,11 @@ export default function Shop() {
       data: [
         { name: "collar", price: "250", image: "https://static.miscota.com/media/1/photos/products/115965/115965-8486886161042_1_g.jpg", stock: 25 },
         { name: "pechera", price: "350", image: "https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/1273/04-1614011179-6321262b8c1d8.jpeg", stock: 15 },
+        { name: "Cama para perro", price: "3000", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPn6Q-FZRnZDuXwUmkPF_oGCB-M7bnEmUiQ&usqp=CAU", stock: 10 },
+
       ],
     };
-    // Aquí arriba se pedirá al back, por ahora invento dos articulos
+    // Aquí arriba se pedirá al back, por ahora invento tres articulos
     setArticles(response.data);
   }, []);
   return (
