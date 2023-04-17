e0c038adaba8596b3e6bf3e7e4a73891774f07c8
aleev97 <valverdealejandro327@gmail.com>
2023-04-17 11:07:28 -0300

    change(component/Histiry) : agregue la historia

--- Front/src/components/History/History.jsx
+++ Front/src/components/History/History.jsx
@@ -3,9 +3,12 @@ import style from "./History.module.css";
 
 export default function History() {
 
-    return(
-        <div>
+    return (
+        <div className={style.container}>
             <h1 className={style.title} >SALVANDO HUELLAS!</h1>
+            <h5 className={style.text} >
+                Hace más de 20 años nos encontramos un grupo de personas en nuestra ciudad y alrededores, trabajando cada uno por su lado en la misma causa, tratar de aliviar y ayudar a los animales necesitados en situación de calle y de barrios carenciados, con necesidades cada vez mayores, entonces nos reunimos y decidimos aunar fuerzas y recursos, trabajar juntos para llegar más lejos, ayudar más, tratar de concientizar a toda la comunidad y luchar para lograr la participación y compromiso estatal también.
+            </h5>
         </div>
     )
 }
