fbd136e33afb7cdebe2d952c5438bee1d707ce93
Mattger10 <matiaszacariaz@hotmail.com>
2023-04-19 03:58:29 -0300

    Cambios en App, Form, Account, LandingPage

--- Front/src/components/Account/Account.jsx
+++ Front/src/components/Account/Account.jsx
@@ -1,92 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import GoogleLogin from "react-google-login";
+import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./Account.module.css";
 
 const Account = () => {
-  const [darkMode, setDarkMode] = useState(false);
   const [editingProfile, setEditingProfile] = useState(false);
   const [auth, setAuth] = useState(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // Cargar el SDK de autenticación de Google
-    if (typeof window.gapi !== "undefined") {
-      window.gapi.load("auth2", () => {
-        window.gapi.auth2.init({
-          client_id:
-            "939538432679-s6j6vrk6uqc5u50didh0kalqelssrog2.apps.googleusercontent.com",
-        }).then((authInstance) => {
-          setAuth(authInstance);
-        });
-      });
-    }
-
-    // Cargar el SDK de Facebook
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId: "1031031684537225",
-        cookie: true,
-        xfbml: true,
-        version: "v12.0",
-      });
-    };
-    (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }, []);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
 
   const toggleEditingProfile = () => {
     setEditingProfile(!editingProfile);
   };
 
-  const logoutGoogle = () => {
-    if (auth !== null) {
-      auth.signOut().then(() => {
-        console.log("User signed out from Google");
-        navigate("/");
-      });
+  const { logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  {
+    if (isLoading) {
+      return <div>Cargando...</div>;
     }
-  };
-
-  const logoutFacebook = () => {
-    FB.logout(function(response) {
-      console.log("User signed out from Facebook");
-      navigate("/");
-    });
-  };
+  }
 
   return (
     <div className={styles.darkMode}>
-    <div className={styles.container}>
-      <h1>Perfil de usuario</h1>
-      <button onClick={toggleDarkMode}>
-        {darkMode ? "Cambiar a modo claro" : "Cambiar a modo oscuro"}
-      </button>
-      <button onClick={toggleEditingProfile}>
-        {editingProfile ? "Cancelar edición" : "Editar perfil"}
-      </button>
-      <button onClick={logoutGoogle} className={styles.logoutButton}>
-        Cerrar sesión de Google
-      </button>
-      <button onClick={logoutFacebook} className={styles.logoutButton}>
-        Cerrar sesión de Facebook
-      </button>
-      <h2>Mis donaciones</h2>
-      {/*Acá se debería mostrar la lista de donaciones del usuario*/}
-      <h2>Mis favoritos</h2>
-      {/*Acá se debería mostrar la lista de favoritos del usuario*/}
-    </div>
+      <div className={styles.container}>
+        <h1>Perfil de usuario</h1>
+        {isAuthenticated && (
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>Email: {user.email}</p>
+          </div>
+        )}
+        <button onClick={toggleEditingProfile}>
+          {editingProfile ? "Cancelar edición" : "Editar perfil"}
+        </button>
+        <button
+          onClick={() => logout({ returnTo: "/" })}
+          className={styles.logoutButton}
+        >
+          Cerrar sesión
+        </button>
+
+        <h2>Mis donaciones</h2>
+        {/*Acá se debería mostrar la lista de donaciones del usuario*/}
+        <h2>Mis favoritos</h2>
+        {/*Acá se debería mostrar la lista de favoritos del usuario*/}
+      </div>
     </div>
   );
 };
