23e4434c2326ea00507542aa8819521b94a122db
Mattger10 <matiaszacariaz@hotmail.com>
2023-04-20 04:57:35 -0300

    cambios de module.css

--- Front/src/components/History/History.jsx
+++ Front/src/components/History/History.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import style from "./History.module.css";
+import styles from "./History.module.css";
 
 export default function History() {
 
     return (
-        <div className={style.container}>
-            <h1 className={style.title} >SALVANDO HUELLAS!</h1>
-            <h5 className={style.text} >
+        <div className={styles.container}>
+            <h1 className={styles.title} >SALVANDO HUELLAS!</h1>
+            <h5 className={styles.text} >
                 Hace más de 20 años nos encontramos un grupo de personas en nuestra ciudad y alrededores,
                 trabajando cada uno por su lado en la misma causa, tratar de aliviar y ayudar a los animales necesitados en situación de calle y de barrios carenciados,
                 con necesidades cada vez mayores, entonces nos reunimos y decidimos aunar fuerzas y recursos, trabajar juntos para llegar más lejos, ayudar más,
                 tratar de concientizar a toda la comunidad y luchar para lograr la participación y compromiso estatal también.
             </h5>
-            <img className={style.img} src="/img/history.png" alt="Image 1" />
+            <img className={styles.img} src="/img/history.png" alt="Image 1" />
         </div>
     )
 }
