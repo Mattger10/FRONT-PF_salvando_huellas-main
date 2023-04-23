1fc0ed6de19a3f4db698d3bb9a6f4d1044da0750
Mattger10 <matiaszacariaz@hotmail.com>
2023-04-23 07:26:32 -0300

    commit

--- Front/src/components/DetailDog/DetailDogs.jsx
+++ Front/src/components/DetailDog/DetailDogs.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { getDetail } from "../../redux/actions";
+import { getDetail, getReferences } from "../../redux/actions";
 import styles from "../DetailDog/DetailDog.module.css";
 
 export default function DetailDogs() {
   const dispatch = useDispatch();
   const DogDeits = useSelector((state) => state.dogDetail);
+  const references = useSelector((state) => state.references);
   const { id } = useParams();
 
   useEffect(() => {
     dispatch(getDetail(id));
+    dispatch(getReferences());
   }, [dispatch, id]);
-  console.log("dogDeits", DogDeits);
 
   return (
     <div className={styles.containerAll}>
-      <div className={styles.huella}>
+      <div className={styles.huella} />
       <div className={styles.container}>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
@@ -41,17 +42,24 @@ export default function DetailDogs() {
               </div>
 
               <div className={styles.description}>
-                <span>Age: {DogDeits.ageD}</span>
-                <span>Sex: {DogDeits.sexD}</span>
-                <span>Size: {DogDeits.sizeD}</span>
+                <span>Edad: {DogDeits.ageD}</span>
+                <span>Sexo: {DogDeits.sexD}</span>
+                <span>Tamaño: {DogDeits.sizeD}</span>
                 <span>Historia: {DogDeits.historyD}</span>
-                <span>References: {DogDeits.referencesD}</span>
                 <Link className={styles.adoptame} to="/dogs">
                   <button className={styles.button}>Atrás</button>
                 </Link>
                 <Link className={styles.adoptame} to="/adopt">
                   <button className={styles.button}>Adóptame</button>
                 </Link>
+              </div>
+              <div>
+                Referencias:
+                <ul>
+                  {references.map((references) => (
+                    <li key={references.id}>{references.textR}</li>
+                  ))}
+                </ul>
               </div>
             </div>
           ) : (
@@ -61,8 +69,6 @@ export default function DetailDogs() {
             </div>
           )}
         </div>
-        <div className={styles.containAdop}></div>
-      </div>
       </div>
     </div>
   );
