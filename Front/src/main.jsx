d5dbbf1ba294c6ba0c29f997d5ae7309ddbe85e7
pablo herrera <valeherreradelarua@gmail.com>
2023-04-25 08:35:23 -0300

    localhosr back

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -10,18 +10,13 @@ import axios from "axios";
 
 //Para el deploy descomentar:
 
-
 //axios.defaults.baseURL = "https://pfsalvandohuellas-production.up.railway.app/"
 
 // Para trabajar local descomentar:
 
  axios.defaults.baseURL = "http://localhost:3001/" 
 
- axios.defaults.baseURL = "https://pfsalvandohuellas-production.up.railway.app/"
 
-// Para trabajar local descomentar:
-
-//axios.defaults.baseURL = "http://localhost:3001/" 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
