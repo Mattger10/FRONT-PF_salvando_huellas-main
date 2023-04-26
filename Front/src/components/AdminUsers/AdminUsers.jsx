4ecfd1f1dc446e380bb5768c37a193e300754530
pablo herrera <valeherreradelarua@gmail.com>
2023-04-26 20:08:28 -0300

    Fix references on DetailDogs

---
+++ Front/src/components/AdminUsers/AdminUsers.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import styles from "./AdminDogs.module.css";
+
+export default function AdminUsers () {
+
+    useEffect(()=>{
+        
+    },[])
+    return <div>
+
+    </div>
+}
