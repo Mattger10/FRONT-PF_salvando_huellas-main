6d35f5d775e31e70b123e3934586163a431a1f87
Valentin <valeherreradelarua@gmail.com>
2023-04-28 20:36:27 -0300

    Add search user by name

--- Front/src/components/AdminUsers/AdminUsers.jsx
+++ Front/src/components/AdminUsers/AdminUsers.jsx
@@ -1,20 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AdminUsers.module.css";
 import { getUsers } from "../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import CardUser from "../CardUser/CardUser";
+import axios from "axios";
 
 export default function AdminUsers() {
   const dispatch = useDispatch();
   const allUsers = useSelector((state) => state.users);
   const navigate = useNavigate();
+  const [searchName, setSearchName] = useState("")
+
+  const banUser = (id)=>{
+    axios.put('/users/ban/'+id)
+    .then(res => dispatch(getUsers()))
+  }
+  const unbanUser = (id)=>{
+    axios.put('/users/unban/'+id)
+    .then(res => dispatch(getUsers()))
+  }
+
+  const handleSearchName = (e)=>{
+    setSearchName(e.target.value)
+  }
 
   useEffect(() => {
     const userLocal = JSON.parse(window.localStorage.getItem("user"));
     if (!userLocal.isAdminU) {
       navigate("/home");
-      
     }
   }, []);
   useEffect(() => {
@@ -35,6 +49,8 @@ export default function AdminUsers() {
           isBan={user.is_ban}
           isAdmin={user.isAdminU}
           reason={user.reasonU}
+          banUser={banUser}
+          unbanUser={unbanUser}
         />
       )})
     : "";
@@ -45,6 +61,7 @@ export default function AdminUsers() {
         <button type="button">Volver</button>
       </Link>
       <h2>Gestionar Usuarios</h2>
+      <input type='search' onChange={handleSearchName} value={searchName} placeholder='Buscar por nombre...'></input>
       <div>{showUsers}</div>
     </div>
   );
