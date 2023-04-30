93fe261d313283a6b8307495c0d006846e114227
pablo herrera <valeherreradelarua@gmail.com>
2023-04-30 04:13:28 -0300

    Guardo cuentas de terceros en la DB

--- Front/src/components/Home/Home.jsx
+++ Front/src/components/Home/Home.jsx
@@ -1,7 +1,54 @@
 import React, { useEffect } from "react";
 import styles from "./Home.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 export default function Home() {
+  const { isAuthenticated, user, isLoading } = useAuth0();
+
+  useEffect(() => {
+    const userLocal = JSON.parse(window.localStorage.getItem("user"));
+    if (!userLocal || !userLocal.nameU) {
+      if (isAuthenticated && !isLoading) {
+        console.log("USER TERCEROS ",user)
+        let exist = false;
+        axios
+          .get("/users")
+          .then((res) => res.data)
+          .then((users) => {
+            users.forEach((userL) => {
+              if (userL.emailU === user.email) {
+                exist = true;
+                window.localStorage.setItem(
+                  "user",
+                  JSON.stringify({ ...userL })
+                );
+              }
+            });
+          })
+          .catch((error) => console.log(error.message));
+        if (!exist) {
+          axios
+            .post("/users/register", {
+              nameU: user.name.split(" ")[0],
+              lastNameU: user.name.split(" ")[1] || " ",
+              passwordU: "huellas",
+              idNumbU: Math.round(Math.random() * 100000000), // DNI del usuario, por ahora random
+              emailU: user.email,
+              phoneU: "Sin teléfono",
+              addressU: "Sin direccion",
+              reasonU: "Reason",
+            })
+            .then((res) => res.data)
+            .then((newUser) =>
+              window.localStorage.setItem("user", JSON.stringify(newUser))
+            )
+            .catch((error) => console.log(error.message));
+        }
+      }
+    }
+  }, [isLoading]);
+
   useEffect(() => {
     //creo carrito vacío en localStorage, si aun no existe
     const carrito = window.localStorage.getItem("carrito");
@@ -14,7 +61,9 @@ export default function Home() {
     //contiene ademas del titulo imagenes que se mostraran automaticamente y las redes sociales para ir
     <div className={styles.container}>
       <div className={styles.huella} />
-      <h1 style={{ fontFamily: 'Cat Paw' }} className={styles.title}>SALVANDO HUELLAS!</h1>
+      <h1 style={{ fontFamily: "Cat Paw" }} className={styles.title}>
+        SALVANDO HUELLAS!
+      </h1>
       <div className={styles.body}>
         <div className={styles.kodfunGaleri}>
           <div
@@ -23,11 +72,36 @@ export default function Home() {
                 "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fcomo-son-los-perros-mestizos.png?alt=media&token=76383096-01f8-422f-9212-82393a524411')",
             }}
           ></div>
-          <div style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2FPerrito.jpg?alt=media&token=e3b2707a-e5e2-4068-9e8e-1bf562b7fddf')" }}></div>
-          <div style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fperro15.jpg?alt=media&token=17bd1d28-ceb2-45af-ab0f-973aad3325b9')" }}></div>
-          <div style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fperros-jugando.jpeg?alt=media&token=a881d562-1861-4d09-bac9-95dcf1441ef2')" }}></div>
-          <div style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fperro14.jpeg?alt=media&token=52b92ebf-87ff-43c9-ae22-c9f14f3e2f56')" }}></div>
-          <div style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fdepositphotos_543005528-stock-photo-labrador-retriever-dog-lying-on.jpg?alt=media&token=7afd997f-419f-45ce-beb2-4a46c076c869')" }}></div>
+          <div
+            style={{
+              backgroundImage:
+                "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2FPerrito.jpg?alt=media&token=e3b2707a-e5e2-4068-9e8e-1bf562b7fddf')",
+            }}
+          ></div>
+          <div
+            style={{
+              backgroundImage:
+                "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fperro15.jpg?alt=media&token=17bd1d28-ceb2-45af-ab0f-973aad3325b9')",
+            }}
+          ></div>
+          <div
+            style={{
+              backgroundImage:
+                "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fperros-jugando.jpeg?alt=media&token=a881d562-1861-4d09-bac9-95dcf1441ef2')",
+            }}
+          ></div>
+          <div
+            style={{
+              backgroundImage:
+                "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fperro14.jpeg?alt=media&token=52b92ebf-87ff-43c9-ae22-c9f14f3e2f56')",
+            }}
+          ></div>
+          <div
+            style={{
+              backgroundImage:
+                "url('https://firebasestorage.googleapis.com/v0/b/salvandohuellas.appspot.com/o/salvandohuellas%2Fdepositphotos_543005528-stock-photo-labrador-retriever-dog-lying-on.jpg?alt=media&token=7afd997f-419f-45ce-beb2-4a46c076c869')",
+            }}
+          ></div>
         </div>
       </div>
       <div className={styles.iconsContainer}>
@@ -69,9 +143,8 @@ export default function Home() {
                   title="Síganos en Instagram"
                 />
               </a>
-
               <a href="mailto:salvandohuellasjesusmaria@gmail.com">
-              <img
+                <img
                   loading="lazy"
                   alt="contacto por correo"
                   height="35"
@@ -80,8 +153,11 @@ export default function Home() {
                   title="contacto por correo"
                 />
               </a>
-
-              <a href="https://api.whatsapp.com/send?phone=5493525418986" target="_blank" className={styles.whatsapp}>
+              <a
+                href="https://api.whatsapp.com/send?phone=5493525418986"
+                target="_blank"
+                className={styles.whatsapp}
+              >
                 <img
                   loading="lazy"
                   alt="Contacto por WhatsApp"
@@ -91,7 +167,6 @@ export default function Home() {
                   title="Contacto por WhatsApp"
                 />
               </a>
-
             </div>
           </div>
         </div>
