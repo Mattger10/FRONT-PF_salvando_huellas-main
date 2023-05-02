05ed3839fb409ba620b33bc0c6bb4f878dd3b423
pablo herrera <valeherreradelarua@gmail.com>
2023-05-02 01:22:20 -0300

    Connect ResetPass backend

--- Front/src/components/ResetPassword/ResetPassword.jsx
+++ Front/src/components/ResetPassword/ResetPassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "./ResetPassword.module.css";
 import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
 
 export default function ResetPassword() {
   const [newPass, setNewPass] = useState("");
@@ -8,6 +9,9 @@ export default function ResetPassword() {
   const navigate = useNavigate();
   const location = useLocation();
   const [passToken, setPassToken] = useState("");
+  const [message, setMessage] = useState("")
+  const [loading, setLoading] = useState(false)
+  const loader = <div className={styles.customloader}></div>
 
   useEffect(() => {
     const querys = new URLSearchParams(location.search);
@@ -20,7 +24,18 @@ export default function ResetPassword() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // solicitud al back
+    setLoading(true)
     if (newPass === confirmNewPass) {
+      axios.put('/users/resetpass/1?token='+passToken, {passwordU: newPass}).then(()=> {
+        setLoading(false)
+        setMessage("Contraseña actualizada!")})
+        .catch(error => {
+          setLoading(false)
+          setMessage("Error al actualizar contraseña")
+        })
+    } else {
+      setLoading(false)
+      setMessage("Las contraseñas no coinciden")
     }
   };
 
@@ -30,22 +45,49 @@ export default function ResetPassword() {
         <form className={styles.form} onSubmit={handleSubmit}>
         <h2>Reestablecer contraseña</h2>
           <input
+          type="password"
             className={styles.input}
             placeholder="Nueva contraseña"
             value={newPass}
             onChange={(e) => setNewPass(e.target.value)}
+            required
           ></input>
           <input
+          type="password"
             className={styles.input2}
             placeholder="Confirmar contraseña"
             value={confirmNewPass}
             onChange={(e) => setConfirmNewPass(e.target.value)}
+            required
           ></input>
           <button 
             className={styles.button}
             type="submit">Confirmar</button>
         </form>
       </div>
+      {loading ? <div className={styles.containerMessage}>
+          <div className={styles.message}>
+            {loader}
+          </div>
+        </div> : ""}
+      {message.length ? (
+        <div className={styles.containerMessage}>
+          <div className={styles.message}>
+            <h3>{message}</h3>
+            <button
+              className={styles.button}
+              onClick={() => {
+                if (message === "Contraseña actualizada!"){
+                  setMessage("")
+                  navigate("/")
+                } else setMessage("")
+              }}
+            >
+              {message === "Contraseña actualizada!" ? "Iniciar sesión" : "Aceptar"}
+            </button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
